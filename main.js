dc121bc8bd6589d6a0c3112f15a0a20444f49cd3
George Campbell <campbell.goe@gmail.com>
2023-07-28 23:04:55 +0100

    Add earth globe

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import ThreeGlobe from 'three-globe';
+import earth from './earth.geo.json'
 
+let mouseX = 0
+let mouseY = 0
+let windowHalfX = window.innerWidth / 2
+let windowHalfY = window.innerHeight / 2
+let myGlobe;
+let controls;
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -7,20 +16,66 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 250;
+
+const ambientLight = new THREE.AmbientLight( 0x101010 ); // soft white ambientLight
+scene.add( ambientLight );
+
+const pointLight = new THREE.PointLight( 0xffffff, 100000, 10000 );
+pointLight.position.set( 200, 200, 200 );
+scene.add( pointLight );
+
+function initGlobe(){
+	myGlobe = new ThreeGlobe({
+		waitForGlobeReady: true,
+		animateIn: true
+	})
+		.globeImageUrl('/earth-2002.png')
+		.pointsData(earth.features);
+
+
+	scene.add(myGlobe);
+}
+
+
+function onMouseMove(event){
+	mouseX = event.clientX - windowHalfX
+	mouseY = event.clientY - windowHalfY
+}
+
+function onResize(){
+	camera.aspect = window.innerWidth / window.innerHeight
+	camera.updateProjectionMatrix()
+	const width = window.innerWidth
+	const height = window.innerHeight
+	windowHalfX = width / 2
+	windowHalfY = height / 2
+	renderer.setSize(width, height)
+}
 
 function animate() {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
+	camera.position.x += (Math.abs(mouseX) <= windowHalfX /2) ? (mouseX / 2 - camera.position.x)*0.005 : 0;
+	camera.lookAt(scene.position)
 	renderer.render( scene, camera );
 }
+
+controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+controls.dynamicDampingFactor = 0.01
+controls.enablePan = false
+controls.minDistance = 125
+controls.maxDistance = 800
+controls.rotateSpeed = 0.8
+controls.zoomSpeed = 1
+controls.autoRotate = false
+controls.minPolarAngle = Math.PI/6
+controls.maxPolarAngle = Math.PI - Math.PI/6
+
+window.addEventListener("resize", onResize, false)
+document.addEventListener("mousemove", onMouseMove)
+initGlobe()
+onResize()
 
 animate();
